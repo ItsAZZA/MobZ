9a5e5cb9a86addd4850e0dd68535ac798a349538
ItsAZZA <ItsAZZA@users.noreply.github.com>
2021-08-28 20:44:35 +0300

    Add slime size attribute

--- src/main/kotlin/com/itsazza/mobz/menu/mobs/MagmaCubeMobMenu.kt
+++ src/main/kotlin/com/itsazza/mobz/menu/mobs/MagmaCubeMobMenu.kt
@@ -1,6 +1,19 @@
 package com.itsazza.mobz.menu.mobs
 
 import com.itsazza.mobz.menu.MobMenu
+import org.bukkit.Material
 import org.bukkit.entity.EntityType
 
-class MagmaCubeMobMenu : MobMenu(EntityType.MAGMA_CUBE)
+class MagmaCubeMobMenu : MobMenu(EntityType.MAGMA_CUBE) {
+    override val buttons = super.buttons.also {
+        it.add(
+            typeSelector(
+                SlimeMobMenu.slimeSizes,
+                "Size",
+                "Magma Cube Size",
+                listOf("Defines the size of the", "magma cream mob"),
+                Material.MAGMA_CREAM
+            )
+        )
+    }
+}
--- src/main/kotlin/com/itsazza/mobz/menu/mobs/SlimeMobMenu.kt
+++ src/main/kotlin/com/itsazza/mobz/menu/mobs/SlimeMobMenu.kt
@@ -1,6 +1,29 @@
 package com.itsazza.mobz.menu.mobs
 
 import com.itsazza.mobz.menu.MobMenu
+import de.themoep.inventorygui.GuiElement
+import org.bukkit.Material
 import org.bukkit.entity.EntityType
 
-class SlimeMobMenu : MobMenu(EntityType.SLIME)
+class SlimeMobMenu : MobMenu(EntityType.SLIME) {
+    override val buttons = super.buttons.also {
+        it.add(
+            typeSelector(
+                slimeSizes,
+                "Size",
+                "Slime Size",
+                listOf("Defines the size of the", "slime mob"),
+                Material.SLIME_BALL
+            )
+        )
+    }
+
+    companion object {
+        val slimeSizes = arrayOf(
+            "Baby",
+            "Medium",
+            "Large",
+            "Giant"
+        )
+    }
+}
