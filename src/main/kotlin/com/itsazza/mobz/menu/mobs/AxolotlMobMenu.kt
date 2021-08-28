7882c556d0977806dd70944fc50f6eb5bc0c7dd1
ItsAZZA <ItsAZZA@users.noreply.github.com>
2021-08-28 20:45:00 +0300

    Rework of axolotl type selector

--- src/main/kotlin/com/itsazza/mobz/menu/mobs/AxolotlMobMenu.kt
+++ src/main/kotlin/com/itsazza/mobz/menu/mobs/AxolotlMobMenu.kt
@@ -1,139 +1,29 @@
 package com.itsazza.mobz.menu.mobs
 
 import com.itsazza.mobz.menu.MobMenu
-import com.itsazza.mobz.util.item
-import de.themoep.inventorygui.GuiStateElement
 import org.bukkit.Material
 import org.bukkit.entity.EntityType
 
 class AxolotlMobMenu : MobMenu(EntityType.AXOLOTL) {
     override val buttons = super.buttons.also {
-        it.add(axolotlTypeSelector)
+        it.add(
+            typeSelector(
+                axolotlTypes,
+                "Variant",
+                "Axolotl Types",
+                listOf("Defines the colors of the", "axolotl mob"),
+                Material.AXOLOTL_BUCKET
+            )
+        )
     }
 
-    private val axolotlTypeSelector: GuiStateElement
-    get() = GuiStateElement(
-        '!',
-        GuiStateElement.State(
-            {
-                data.removeKey("Variant")
-            },
-            "0",
-            Material.AXOLOTL_BUCKET.item,
-            "§6§lAxolotl Type",
-            "§0 ",
-            "§7Defines the color of",
-            "§7the axolotl mob",
-            "§0 ",
-            "§2➤ Random",
-            "§8Pink",
-            "§8Brown",
-            "§8Gold",
-            "§8Light Blue",
-            "§8Purple",
-            "§0 ",
-            "§eClick to toggle!"
-        ),
-        GuiStateElement.State(
-            {
-                data.setInteger("Variant", 0)
-            },
-            "0",
-            Material.AXOLOTL_BUCKET.item,
-            "§6§lAxolotl Type",
-            "§0 ",
-            "§7Defines the color of",
-            "§7the axolotl mob",
-            "§0 ",
-            "§8Random",
-            "§2➤ Pink",
-            "§8Brown",
-            "§8Gold",
-            "§8Light Blue",
-            "§8Purple",
-            "§0 ",
-            "§eClick to toggle!"
-        ),
-        GuiStateElement.State(
-            {
-                data.setInteger("Variant", 1)
-            },
-            "1",
-            Material.AXOLOTL_BUCKET.item,
-            "§6§lAxolotl Type",
-            "§0 ",
-            "§7Defines the color of",
-            "§7the axolotl mob",
-            "§0 ",
-            "§8Random",
-            "§8Pink",
-            "§2➤ Brown",
-            "§8Gold",
-            "§8Light Blue",
-            "§8Purple",
-            "§0 ",
-            "§eClick to toggle!"
-        ),
-        GuiStateElement.State(
-            {
-                data.setInteger("Variant", 2)
-            },
-            "2",
-            Material.AXOLOTL_BUCKET.item,
-            "§6§lAxolotl Type",
-            "§0 ",
-            "§7Defines the color of",
-            "§7the axolotl mob",
-            "§0 ",
-            "§8Random",
-            "§8Pink",
-            "§8Brown",
-            "§2➤ Gold",
-            "§8Light Blue",
-            "§8Purple",
-            "§0 ",
-            "§eClick to toggle!"
+    companion object {
+        private val axolotlTypes = arrayOf (
+            "Pink",
+            "Brown",
+            "Gold",
+            "Light Blue",
+            "Purple"
         )
-        ,
-        GuiStateElement.State(
-            {
-                data.setInteger("Variant", 3)
-            },
-            "3",
-            Material.AXOLOTL_BUCKET.item,
-            "§6§lAxolotl Type",
-            "§0 ",
-            "§7Defines the color of",
-            "§7the axolotl mob",
-            "§0 ",
-            "§8Random",
-            "§8Pink",
-            "§8Brown",
-            "§8Gold",
-            "§2➤ Light Blue",
-            "§8Purple",
-            "§0 ",
-            "§eClick to toggle!"
-        ),
-        GuiStateElement.State(
-            {
-                data.setInteger("Variant", 4)
-            },
-            "4",
-            Material.AXOLOTL_BUCKET.item,
-            "§6§lAxolotl Type",
-            "§0 ",
-            "§7Defines the color of",
-            "§7the axolotl mob",
-            "§0 ",
-            "§8Random",
-            "§8Pink",
-            "§8Brown",
-            "§8Gold",
-            "§8Light Blue",
-            "§2➤ Purple",
-            "§0 ",
-            "§eClick to toggle!"
-        )
-    )
+    }
 }
