4fa4cc2f2a0fbcdd48ca6da4b8753240ab75a9e1
ItsAZZA <ItsAZZA@users.noreply.github.com>
2021-08-28 20:44:48 +0300

    Add cat type attribute

--- src/main/kotlin/com/itsazza/mobz/menu/mobs/CatMobMenu.kt
+++ src/main/kotlin/com/itsazza/mobz/menu/mobs/CatMobMenu.kt
@@ -2,10 +2,40 @@ package com.itsazza.mobz.menu.mobs
 
 import com.itsazza.mobz.menu.MobMenu
 import com.itsazza.mobz.util.BasicMobAttribute
+import de.themoep.inventorygui.GuiElement
+import org.bukkit.Material
 import org.bukkit.entity.EntityType
 
 class CatMobMenu : MobMenu(EntityType.CAT) {
     override val basicMobAttributes = super.basicMobAttributes.also {
         it.add(BasicMobAttribute.BABY)
+    }
+
+    override val buttons: MutableList<GuiElement> = super.buttons.also {
+        it.add(
+            typeSelector(
+                catTypes,
+                "CatType",
+                "Cat Type",
+                listOf("Defines the color of the", "cat mob"),
+                Material.CAT_SPAWN_EGG
+            )
+        )
+    }
+
+    companion object {
+        private val catTypes = arrayOf (
+            "Tabby",
+            "Tuxedo",
+            "Red Tabby",
+            "Siamese",
+            "British Shorthaird",
+            "Calico",
+            "Persian",
+            "Ragdoll",
+            "White",
+            "Jellie",
+            "Black"
+        )
     }
 }
