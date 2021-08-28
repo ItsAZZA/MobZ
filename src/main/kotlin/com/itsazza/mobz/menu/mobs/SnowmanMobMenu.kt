936565f2c24fc0e97764b692c8b17e1e99cb25fa
ItsAZZA <ItsAZZA@users.noreply.github.com>
2021-08-28 20:44:26 +0300

    Add snowman pumpkin attribute

--- src/main/kotlin/com/itsazza/mobz/menu/mobs/SnowmanMobMenu.kt
+++ src/main/kotlin/com/itsazza/mobz/menu/mobs/SnowmanMobMenu.kt
@@ -1,9 +1,13 @@
 package com.itsazza.mobz.menu.mobs
 
 import com.itsazza.mobz.menu.MobMenu
+import com.itsazza.mobz.util.BasicMobAttribute
 import de.tr7zw.changeme.nbtapi.NBTContainer
 import org.bukkit.entity.EntityType
 
 class SnowmanMobMenu : MobMenu(EntityType.SNOWMAN) {
     override val data: NBTContainer = NBTContainer("{EntityTag:{id:\"${mobType.key.toString().replace("minecraft:", "")}\"}}")
+    override val basicMobAttributes = super.basicMobAttributes.also {
+        it.add(BasicMobAttribute.SNOWMAN_PUMPKIN)
+    }
 }
