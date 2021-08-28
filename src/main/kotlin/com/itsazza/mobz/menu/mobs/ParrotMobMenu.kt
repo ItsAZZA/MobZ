e0b84ecff3b0f7f9e2f0da1dc9c47b6d08f9a2dc
ItsAZZA <ItsAZZA@users.noreply.github.com>
2021-08-28 20:43:50 +0300

    Add parrot type

--- src/main/kotlin/com/itsazza/mobz/menu/mobs/ParrotMobMenu.kt
+++ src/main/kotlin/com/itsazza/mobz/menu/mobs/ParrotMobMenu.kt
@@ -1,6 +1,29 @@
 package com.itsazza.mobz.menu.mobs
 
 import com.itsazza.mobz.menu.MobMenu
+import org.bukkit.Material
 import org.bukkit.entity.EntityType
 
-class ParrotMobMenu : MobMenu(EntityType.PARROT)
+class ParrotMobMenu : MobMenu(EntityType.PARROT) {
+    override val buttons = super.buttons.also {
+        it.add(
+            typeSelector(
+                parrotColors,
+                "Variant",
+                "Parrot Color",
+                listOf("Defines the colors of", "the parrot mob"),
+                Material.PARROT_SPAWN_EGG
+            )
+        )
+    }
+
+    companion object {
+        private val parrotColors = arrayOf(
+            "Red",
+            "Blue",
+            "Green",
+            "Cyan",
+            "Gray"
+        )
+    }
+}
