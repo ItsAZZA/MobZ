979dbdd82ea22fa0d7f6f4bad9e9603f9956d42c
ItsAZZA <ItsAZZA@users.noreply.github.com>
2021-08-28 20:43:38 +0300

    Add Llama type and strength

--- src/main/kotlin/com/itsazza/mobz/menu/mobs/LlamaMobMenu.kt
+++ src/main/kotlin/com/itsazza/mobz/menu/mobs/LlamaMobMenu.kt
@@ -2,6 +2,7 @@ package com.itsazza.mobz.menu.mobs
 
 import com.itsazza.mobz.menu.MobMenu
 import com.itsazza.mobz.util.BasicMobAttribute
+import org.bukkit.Material
 import org.bukkit.entity.EntityType
 
 class LlamaMobMenu: MobMenu(EntityType.LLAMA) {
@@ -9,5 +10,35 @@ class LlamaMobMenu: MobMenu(EntityType.LLAMA) {
         it.add(BasicMobAttribute.TAME)
         it.add(BasicMobAttribute.WITH_CHEST)
         it.add(BasicMobAttribute.BABY)
+    }
+
+    override val buttons = super.buttons.also {
+        it.add(
+            typeSelector(
+                llamaTypes,
+                "Variant",
+                "Llama Type",
+                listOf("Defines the color of the", "llama mob"),
+                Material.LLAMA_SPAWN_EGG
+            )
+        )
+        it.add(
+            typeSelector(
+                (1..5).map {int -> int.toString() }.toList().toTypedArray(),
+                "Strength",
+                "Llama Strength",
+                listOf("Defines the strength", "of the llama mob"),
+                Material.IRON_BLOCK
+            )
+        )
+    }
+
+    companion object {
+        val llamaTypes = arrayOf (
+            "Cream",
+            "White",
+            "Brown",
+            "Gray"
+        )
     }
 }
--- src/main/kotlin/com/itsazza/mobz/menu/mobs/TraderLlamaMobMenu.kt
+++ src/main/kotlin/com/itsazza/mobz/menu/mobs/TraderLlamaMobMenu.kt
@@ -2,10 +2,23 @@ package com.itsazza.mobz.menu.mobs
 
 import com.itsazza.mobz.menu.MobMenu
 import com.itsazza.mobz.util.BasicMobAttribute
+import org.bukkit.Material
 import org.bukkit.entity.EntityType
 
 class TraderLlamaMobMenu : MobMenu(EntityType.TRADER_LLAMA) {
     override val basicMobAttributes = super.basicMobAttributes.also {
         it.add(BasicMobAttribute.TAME)
+    }
+
+    override val buttons = super.buttons.also {
+        it.add(
+            typeSelector(
+                LlamaMobMenu.llamaTypes,
+                "Variant",
+                "Llama Type",
+                listOf("Defines the color of", "the trader llama mob"),
+                Material.TRADER_LLAMA_SPAWN_EGG
+            )
+        )
     }
 }
