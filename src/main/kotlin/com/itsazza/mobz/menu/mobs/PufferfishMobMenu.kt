da4d62a5bddc8359f8fc70081200883a4770a85f
ItsAZZA <ItsAZZA@users.noreply.github.com>
2021-08-28 20:44:02 +0300

    Add pufferpish puff state

--- src/main/kotlin/com/itsazza/mobz/menu/mobs/PufferfishMobMenu.kt
+++ src/main/kotlin/com/itsazza/mobz/menu/mobs/PufferfishMobMenu.kt
@@ -1,6 +1,28 @@
 package com.itsazza.mobz.menu.mobs
 
 import com.itsazza.mobz.menu.MobMenu
+import org.bukkit.Material
 import org.bukkit.entity.EntityType
 
-class PufferfishMobMenu : MobMenu(EntityType.PUFFERFISH)
+class PufferfishMobMenu : MobMenu(EntityType.PUFFERFISH) {
+    override val buttons = super.buttons.also {
+        it.add(
+            typeSelector(
+               pufferFishStates,
+               "PuffState",
+                "Puff State",
+                listOf("Defines the puff state of", "the pufferfish mob"),
+                Material.PUFFERFISH,
+                "Normal",
+                1
+            )
+        )
+    }
+
+    companion object {
+        private val pufferFishStates = arrayOf(
+            "Partial Buff",
+            "Full Buff"
+        )
+    }
+}
